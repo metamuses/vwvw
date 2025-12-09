37ecc94f4b0c2669c1aa9807a4a7d6826ffac380
Tommaso Barbato <tommasobarbato@gmail.com>
2025-12-09 16:29:31 +0100

    Split

--- js/item.js
+++ js/item.js
@@ -117,7 +117,7 @@ export function initItemPage() {
               }
             });
 
-            rowOtherNarr.style.display = "";
+            rowOtherNarrative.style.display = "";
           }
         }
 
